0713b7d066ab026789cbfefc7d59899a8cb59c11
1heart <yixin1996@gmail.com>
2022-05-10 15:23:31 -0700

    Add detection

--- src/graspnet_baseline/mrp_wrapper.py
+++ src/graspnet_baseline/mrp_wrapper.py
@@ -20,7 +20,7 @@ class GraspNet1BServer(GraspServer):
         # GraspNet-1Billion requires flipping before processing, and undoing the transformation
         self.flip = np.array([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
 
-    def _get_grasps(self, pcd: open3d.geometry.PointCloud) -> graspnetAPI.GraspGroup:
+    def _get_grasps(self, pcd: open3d.geometry.PointCloud, scene_pcd: open3d.geometry.PointCloud=None) -> graspnetAPI.GraspGroup:
         pcd = pcd.transform(self.flip)
         points, colors = np.asarray(pcd.points), np.asarray(pcd.colors)
 
@@ -28,16 +28,32 @@ class GraspNet1BServer(GraspServer):
         grasp_group = get_grasps(self.net, end_points)
 
         # Remove any grasp candidates that have collisions.
+        prev_num_grasps = len(grasp_group)
         if self.net_cfg.collision_thresh > 0:
-            grasp_group = collision_detection(
-                grasp_group, np.array(cloud.points), cfgs=self.net_cfg
-            )
-
+            if scene_pcd is None:
+                grasp_group = collision_detection(
+                    grasp_group, np.array(cloud.points), cfgs=self.net_cfg
+                )
+            else:
+                grasp_group = collision_detection(
+                    grasp_group, np.array(scene_pcd.points), cfgs=self.net_cfg
+                )
+            if len(grasp_group) < prev_num_grasps:
+                print(f"Removed {prev_num_grasps - len(grasp_group)} grasps due to collisions")
         grasp_group.nms()
         grasp_group.sort_by_score()
         grasp_group.transform(self.flip)
 
         return grasp_group
+
+    def _get_collisions(self, grasps: graspnetAPI.GraspGroup, scene_pcd: open3d.geometry.PointCloud):
+        print("Filtering grasps...")
+        filtered_grasps = collision_detection(
+            grasps, np.array(scene_pcd.points), cfgs=self.net_cfg
+        )
+        print(f"Number of filtered grasps: {len(grasps) - len(filtered_grasps)}/{len(grasps)}")
+        return filtered_grasps
+
 
 
 if __name__ == "__main__":
